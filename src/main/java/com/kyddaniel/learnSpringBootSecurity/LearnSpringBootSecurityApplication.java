e48ca96017cfd8c25eb48ae36f6542bf05315134
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-06 23:18:31 +0800

    Initial commit

---
+++ src/main/java/com/kyddaniel/learnSpringBootSecurity/LearnSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.learnSpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyddaniel/learnSpringBootSecurity/LearnSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.learnSpringBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
